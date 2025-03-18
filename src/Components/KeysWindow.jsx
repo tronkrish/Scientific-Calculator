4cb0fe82ba5b1662f0a318fea293098b49de9a4f
tronkrish <sriprasanna8585@gmail.com>
2025-03-18 11:29:58 +0530

    first commit

---
+++ src/Components/KeysWindow.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const KeysWindow = ({handleButton}) => {
+    const sciKeys = ["sin", "cos", "ln", "log", "tan", "π", "e", "^", "!", "√"] ;
+
+    const basicKeys = [
+        "7", "8", "9", "*", "/", "4", "5", "6", "-", "(", "1", "2", "3", "+", ")", ".", "0", "DEL", "AC", "=",
+    ] ;
+  return (
+    <div className='keysWindow'>
+        <div className="keys-scientific">
+            {sciKeys.map((item, index) => (
+                <button key={index} onClick={()=> handleButton(item)}>{item}</button>
+            ))}
+        </div>
+
+        <div className="line"></div>
+
+        <div className="keys-basic">
+            {basicKeys.map((item, index) => (
+                <button key={index} className= {`${item >= "0" && item <="9" ? "number" : ""} ${item === "=" && "equal"}`} onClick={()=> handleButton(item)}>{item}</button>
+            ))}
+        </div>
+    </div>
+  )
+}
+
+export default KeysWindow
